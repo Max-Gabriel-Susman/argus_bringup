94cef3fa8bad5f0d8cec6e5f76a4bff93bed73df
Max-Gabriel-Susman <gabe.susman@gmail.com>
2026-02-04 06:09:12 -0700

    revised launch file to adhere to flake8 style conventions

---
+++ README.md
@@ -0,0 +1,13 @@
+# Argus Bringup
+
+This repository contains the bringup package for the Argus cybernetics stack
+
+## Local Usage
+
+## Development
+
+To test locally before you hit the CI use: 
+```
+colcon test --packages-select argus_bringup
+colcon test-result --verbose
+```
--- launch/poc_inference_effectors.launch.py
+++ launch/poc_inference_effectors.launch.py
@@ -5,31 +5,30 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration("use_sim_time")
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
     inference_node = Node(
-        package="argus_inference",
-        executable="inference_node",
-        name="argus_inference",
-        output="screen",
-        parameters=[{"use_sim_time": use_sim_time}],
+        package='argus_inference',
+        executable='inference_node',
+        name='argus_inference',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
     )
 
     effectors_node = Node(
-        package="argus_effectors",
-        executable="effectors_node",
-        name="argus_effectors",
-        output="screen",
-        parameters=[{"use_sim_time": use_sim_time}],
+        package='argus_effectors',
+        executable='effectors_node',
+        name='argus_effectors',
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}],
     )
 
     return LaunchDescription([
         DeclareLaunchArgument(
-            "use_sim_time",
-            default_value="false",
-            description="Use simulation time if true",
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation time if true',
         ),
         inference_node,
         effectors_node,
     ])
-
